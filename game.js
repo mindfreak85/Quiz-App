e5b1b4b96b2a24df44f35f3023b691ea67f0ce0a
mindfreak85 <rifleyuen@yahoo.com.sg>
2024-07-11 21:06:48 +0800

    Added end.html and end.js page

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById("questionCounter");
+const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -53,12 +54,15 @@ getNewQuestion = () => {
 
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS)
     {
+        localStorage.setItem('mostRecentScore', score);
         //go to end html page
         return window.location.assign("/end.html");
     }
 
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`; 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
