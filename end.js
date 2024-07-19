18b48eaf2f740ed4ebdc44401b581964f3cca421
mindfreak85 <rifleyuen@yahoo.com.sg>
2024-07-19 19:32:30 +0800

    Added feature to have local storage of scores

--- end.js
+++ end.js
@@ -2,6 +2,10 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+
+const MAX_HIGH_SCORES = 5;
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -11,4 +15,15 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
     console.log("click save button");
     e.preventDefault();
+
+    const score = {
+        score: Math.floor( Math.random() * 100 ),
+        name: username.value,
+    };
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('index.html');
 }
